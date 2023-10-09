450a5bacb2a7fc98d37954ac2176acb1e84096b6
Lucas Cordeiro <joaolucascttprofissional@gmail.com>
2023-10-09 17:41:33 -0300

    particles effect

--- pages/index.js
+++ pages/index.js
@@ -67,16 +67,14 @@ const Home = () => {
           className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat 
           w-full h-full absolute mix-blend-color-dodge translate-z-0"></div>
         {/* efeito de particulas */}
-        <div>
-          particulas
-        </div>
+        <ParticlesContainer />
         {/* meu avatar */}
         <motion.div
           variants={fadeIn('up', 0.5)}
           initial="hidden"
           animate="show"
           exit="hidden"
-          transition={{duration: 1, ease: 'easeInOut'}}
+          transition={{ duration: 1, ease: 'easeInOut' }}
           className="w-full h-full max-w-[537px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]"
         >
           <Avatar />
