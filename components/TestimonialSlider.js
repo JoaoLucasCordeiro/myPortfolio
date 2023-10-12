2d2f06b9da07f139a7194e7bf19302883425e887
Lucas Cordeiro <joaolucascttprofissional@gmail.com>
2023-10-11 21:49:34 -0300

    testimonials section

--- components/TestimonialSlider.js
+++ components/TestimonialSlider.js
@@ -1,30 +1,86 @@
+import { Swiper, SwiperSlide } from "swiper/react"
+
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+
+import { Navigation, Pagination } from "swiper"
+import { FaQuoteLeft } from "react-icons/fa"
+import Image from "next/image"
+
+
 // testimonial data
 const testimonialData = [
   {
     image: '/t-avt-1.png',
-    name: 'Anne Smith',
-    position: 'Customer',
+    name: 'Mateus Curvelo',
+    position: 'Cliente',
     message:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
+      'Excelente profissional, atencioso aos detalhes, me surpreendeu na parte de designe e VFX do meu site, sempre esteve disposto a me ouvir e apresentar suas ideias ao projeto, recomendo bastante, reitero, um excelente profissional!',
   },
   {
     image: '/t-avt-2.png',
-    name: 'Jane Doe',
-    position: 'Customer',
+    name: 'Ivaldir Junior',
+    position: 'P.O',
     message:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
   },
   {
     image: '/t-avt-3.png',
-    name: 'Jhon Doe',
-    position: 'Customer',
+    name: 'Rafael Roque',
+    position: 'Tech leader',
     message:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
   },
 ];
 
+
 const TestimonialSlider = () => {
-  return <div>Testimonial Slider</div>;
+  return (
+    <Swiper
+      navigation={true}
+      spaceBetween={10}
+      pagination={{ clickable: true }}
+      modules={[Navigation, Pagination]}
+      className="h-[400px] "
+    >
+      {testimonialData.map((person, index) => {
+        return (
+          <SwiperSlide key={index}>
+            <div className="flex flex-col items-center md:flex-row gap-x-8 h-full px-16">
+              {/* avatar, nome, posicao */}
+              <div className="w-full max-w-[300px] flex flex-col xl:justify-center items-center relative mx-auto xl:mx-0">
+                <div className="flex flex-col justify-center text-center ">
+                  {/* avatar */}
+                  <div className="mb-2 mx-auto ">
+                    <Image src={person.image} width={100} height={100} alt="" />
+                  </div>
+                  {/* nome */}
+                  <div className="text-lg">{person.name}</div>
+                  {/* position */}
+                  <div className="text-[12px] uppercase font-extralight tracking-widest">{person.position}</div>
+                </div>
+              </div>
+
+              {/* msg */}
+
+              <div className="flex-1 flex flex-col
+               justify-center before:w-[1px] xl:before:bg-white/20 
+               xl:before:absolute xl:before:left-0 xl:before:h-[200px] 
+               relative xl:pl-20">
+                <div className="mb-4">
+                  <FaQuoteLeft className="text-4xl xl:text-6xl text-white/20 mx-auto md:mx-0" />
+                </div>
+                <div className="xl:text-lg text-center md:text-left ">{person.message}</div>
+              </div>
+            </div>
+          </SwiperSlide>
+        )
+      })
+      }
+    </Swiper >
+  )
 };
 
 export default TestimonialSlider;
+
--- pages/testimonials/index.js
+++ pages/testimonials/index.js
@@ -1,5 +1,31 @@
+import TestimonialSlider from "../../components/TestimonialSlider"
+
+import { motion } from "framer-motion"
+import { fadeIn } from "../../variants"
+
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  return (
+    <div className="h-full bg-primary/60 py-32 text-center">
+      <div className="container mx-auto h-full flex flex-col justify-center">
+        {/* titulo */}
+        <motion.h2
+          variants={fadeIn('up', 0.2)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          className="text-4xl font-bold mb-8 xl:mb-0">Veja o que <span className="text-accent">clientes</span> e pessoas que <span className="text-accent">trabalharam comigo</span> tem a dizer <span className="text-accent">.</span></motion.h2>
+        {/* slider */}
+        <motion.div
+          variants={fadeIn('up', 0.4)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+        >
+          <TestimonialSlider />
+        </motion.div>
+      </div>
+    </div>
+  )
 };
 
 export default Testimonials;
